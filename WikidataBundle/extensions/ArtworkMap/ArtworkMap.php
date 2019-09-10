674586c20bca4658fa7c3fb0d2153a198087dd0e
François Le Runigo <lerunigo@gmail.com>
2019-09-10 10:06:00 +0200

    [AM-01] Suppression répertoire dupliqué

--- WikidataBundle/extensions/ArtworkMap/ArtworkMap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'ArtworkMap' );
-	$wgMessagesDirs['ArtworkMap'] = __DIR__ . '/i18n';
-	return true;
-} else {
-	die( 'This version of the ArtworkMap extension requires MediaWiki 1.25+' );
-}
--- WikidataBundle/utils/js/config.js
+++
@@ -1,9 +0,0 @@
-const constants = {
-  url: {
-    getMap: 'extensions/WikidataBundle/utils/php/getMap.php',
-  },
-}
-
-console.log('???')
-
-export default constants
